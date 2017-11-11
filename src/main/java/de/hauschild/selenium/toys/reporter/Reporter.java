b17dfee530b11a422ae9f6441d7e4f95d539718a
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2017-11-11 20:11:47 +0100

    the reporter collects all tests (separated in succeeded, failed, skiped)

--- src/main/java/de/hauschild/selenium/toys/reporter/Reporter.java
+++ src/main/java/de/hauschild/selenium/toys/reporter/Reporter.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.testng.IReporter;
 import org.testng.ISuite;
+import org.testng.TestListenerAdapter;
 import org.testng.xml.XmlSuite;
 
-public class Reporter implements IReporter {
+public class Reporter extends TestListenerAdapter implements IReporter {
 
   @Override
   public void generateReport(final List<XmlSuite> xmlSuites, final List<ISuite> suites,
-      final String outputDirectory) {
-    System.out.println("It's alive!!!");
-  }
+      final String outputDirectory) {}
 
 }
