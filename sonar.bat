365e4cdbdb748d28da71f4b874733bca44288aeb
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2017-11-23 18:39:59 +0100

    remove sonar.bat

--- sonar.bat
+++
@@ -1,1 +0,0 @@
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=klaushauschild1984-github -Dsonar.login=b76d5afc783cdadb997159de08e925aa1598b4df
