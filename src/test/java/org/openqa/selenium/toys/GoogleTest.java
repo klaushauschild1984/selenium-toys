4d102822a8688fb797e89156cd5ac9c1b36c697b
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2017-11-27 00:11:37 +0100

    temporary disable test case

--- src/test/java/org/openqa/selenium/toys/GoogleTest.java
+++ src/test/java/org/openqa/selenium/toys/GoogleTest.java
@@ -5,7 +5,6 @@ import static org.openqa.selenium.toys.Webdriver.IMPLICITLY_WAIT;
 import static org.openqa.selenium.toys.factory.chrome.ChromeWebdriverFactory.WORK_DIRECTORY;
 
 import org.openqa.selenium.By;
-import org.testng.annotations.Test;
 
 @Webdriver(value = CHROME, options = { //
     WORK_DIRECTORY, ".", //
@@ -14,7 +13,7 @@ import org.testng.annotations.Test;
 @EntryPoint("http://www.google.com")
 public class GoogleTest extends SeleniumTestNGTests {
 
-  @Test
+  // @Test
   public void failingCalculatorTest() {
     type("2+2") //
         .on(By.id("lst-ib")) //
